b2543f3c55dc7ea9abbf840756aa0e2bc12402bf
Al <you@example.com>
2017-05-10 18:26:54 +0700

    Add visual view of calendar

--- dist/components/Calendar.jsx
+++ dist/components/Calendar.jsx
@@ -3,16 +3,34 @@ import { connect } from 'redux';
 import moment from 'moment';
 import initialDates from '../../config/configData.json';
 
-const renderListDate = (dates) => {
+const formateDates = (dates) => {
   return Object.keys(dates).map((data) => {
     const momentObj = moment(data, 'DD-MM-YYYY').locale('ru');
     return [momentObj.format('MMM'), momentObj.date(), momentObj.format('ddd')];
-  })
-}
-console.log(renderListDate(initialDates))
+  });
+};
+
 class Calendar extends React.Component {
+  renderCalendar() {
+    const { dates } = this.props;
+    return formateDates(initialDates).map((date) => {
+      return (
+        <li key={date[1]} className="calendar-list-item">
+          <div className="date-month">{date[0]}</div>
+          <div className="date-day">{date[1]}</div>
+          <div className="date-week">{date[2]}</div>
+        </li>
+      )
+    });
+  }
   render() {
-    return (null)
+    return (
+      <div className="calendar">
+        <ul className="calendar-list">
+          {this.renderCalendar()}
+        </ul>
+      </div>
+    )
   }
 }
 export default Calendar;
