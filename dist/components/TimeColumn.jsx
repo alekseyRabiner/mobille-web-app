3c43aa8ae06518ee592593b378300e4fcb75685f
Al <you@example.com>
2017-05-13 01:57:26 +0700

    Rewrite render

--- dist/components/TimeColumn.jsx
+++ dist/components/TimeColumn.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import arrayOfTime from '../../config/ArrayOfTime.json';
 
 const renderTimeColumn = (dataOfTime) => {
   return dataOfTime.map((time) => {
+    const key = `${time[0]}-${time[1]}`;
     return (
-      <li className="timecolumn-list-item">
+      <li key={key} className="timecolumn-list-item">
         <div className="timecolumn-hour">{time[0]}</div>
         <div className="timecolumn-minute">{time[1]}</div>
       </li>
@@ -12,7 +12,7 @@ const renderTimeColumn = (dataOfTime) => {
   });
 };
 
-const TimeColumn = () => {
+const TimeColumn = ({arrayOfTime}) => {
   return (
     <div className="timecolumn">
       <ul className="timecolumn-list">
